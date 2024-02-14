91a5e9454ffaf383ffc7c5d129b37bd10736b492
vishalbhoriwal <vishalkumarbhoriwal@gmail.com>
2024-02-14 19:15:55 +0530

    Update set matrix zerp

--- set_matrix_zero.js
+++ set_matrix_zero.js
@@ -1,1 +1,29 @@
-console.log('set matrix zero');
+// url : https://leetcode.com/problems/set-matrix-zeroes/description/
+// Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+// Output: [[1,0,1],[0,0,0],[1,0,1]]
+
+
+var setZeroes = function (matrix) {
+  let rows = [], cols = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] === 0) {
+        rows.push(i);
+        cols.push(j);
+      }
+    }
+  }
+  for (let i = 0; i < rows.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      matrix[rows[i]][j] = 0;
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < cols.length; j++) {
+      matrix[i][cols[j]] = 0;
+    }
+  }
+  return matrix;
+};
+
+console.log(setZeroes([[1,1,1],[1,0,1],[1,1,1]]))
